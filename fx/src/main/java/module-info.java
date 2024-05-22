9361311807020c3998cca45b290633bf88a88551
AGanzer <ganand@web.de>
2024-05-22 16:57:42 +0200

    refactored

--- fx/src/main/java/module-info.java
+++ fx/src/main/java/module-info.java
@@ -2,7 +2,5 @@ module de.ganzer.fx {
     requires javafx.controls;
     requires javafx.fxml;
     requires ganzer.core.main;
-    opens de.ganzer.fx to javafx.fxml;
-    exports de.ganzer.fx;
     exports de.ganzer.fx.validation;
 }
