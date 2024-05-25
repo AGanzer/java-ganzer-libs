081347119cc88062ccb5f865293907a2ee8776d6
AGanzer <ganand@web.de>
2024-05-25 10:44:32 +0200

    removed unused imports

--- core/src/test/java/de/ganzer/core/files/FileNamesTest.java
+++ core/src/test/java/de/ganzer/core/files/FileNamesTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
