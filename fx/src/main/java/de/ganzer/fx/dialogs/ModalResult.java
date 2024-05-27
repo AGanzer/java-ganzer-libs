ea17bc5c85b240478c1a866ad38fcd05681c38d6
AGanzer <ganand@web.de>
2024-05-27 20:30:27 +0200

    added java doc

--- fx/src/main/java/de/ganzer/fx/dialogs/ModalResult.java
+++ fx/src/main/java/de/ganzer/fx/dialogs/ModalResult.java
@@ -6,13 +6,49 @@ package de.ganzer.fx.dialogs;
  */
 @SuppressWarnings("unused")
 public class ModalResult {
+    /**
+     * The result of a "Cancel" Button.
+     */
     public static final int CANCEL = 0;
+
+    /**
+     * The result of an "OK" Button.
+     */
     public static final int OK = 1;
+
+    /**
+     * The result of a "Yes" Button.
+     */
     public static final int YES = 2;
+
+    /**
+     * The result of a "Yes To All" Button.
+     */
     public static final int YES_TO_ALL = 3;
+
+    /**
+     * The result of a "No" Button.
+     */
     public static final int NO = 4;
+
+    /**
+     * The result of a "No To All" Button.
+     */
     public static final int NO_TO_ALL = 5;
+
+    /**
+     * The result of a "Retry" Button.
+     */
     public static final int RETRY = 6;
+
+    /**
+     * The result of an "Abort" Button.
+     */
     public static final int ABORT = 7;
+
+    /**
+     * The result of an "Ignore" Button.
+     */
     public static final int IGNORE = 8;
 }
+
