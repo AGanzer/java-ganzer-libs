b000fa4339964c9ad4f2f51a7770640d4d5d6462
AGanzer <ganand@web.de>
2024-09-25 17:05:51 +0200

    fixed unusable BOMInputStreamReader because the StreamDecoder ist not exported since Java 9

--- core/src/main/java/de/ganzer/core/io/BOMInputStreamReader.java
+++ core/src/main/java/de/ganzer/core/io/BOMInputStreamReader.java
@@ -1,10 +1,11 @@
 package de.ganzer.core.io;
 
-import sun.nio.cs.StreamDecoder;
-
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.*;
 import java.util.Objects;
 
 /**
@@ -46,7 +47,7 @@ public class BOMInputStreamReader extends Reader {
      */
     public BOMInputStreamReader(InputStream in) throws IOException {
         super(in);
-        this.in = new PushbackInputStream(in);
+        this.in = new PushbackInputStream(in, 4);
         this.sd = getDecoder(Charset.defaultCharset());
     }
 
@@ -63,7 +64,7 @@ public class BOMInputStreamReader extends Reader {
      */
     public BOMInputStreamReader(InputStream in, String charsetName) throws IOException {
         super(in);
-        this.in = new PushbackInputStream(in);
+        this.in = new PushbackInputStream(in, 4);
         this.sd = getDecoder(Charset.forName(charsetName));
     }
 
@@ -78,7 +79,7 @@ public class BOMInputStreamReader extends Reader {
      */
     public BOMInputStreamReader(InputStream in, Charset cs) throws IOException {
         super(in);
-        this.in = new PushbackInputStream(in);
+        this.in = new PushbackInputStream(in, 4);
         this.sd = getDecoder(cs);
     }
 
@@ -202,14 +203,14 @@ public class BOMInputStreamReader extends Reader {
         byte[] buff = new byte[4];
         int read = in.read(buff, 0, 4);
 
-        if (read == -1)
+        if (read < 1)
             return BOM.NONE;
 
         BOM bom = BOM.getBOM(buff);
-        int writeBack = read - bom.getNumBytes();
+        int bytesLeft = read - bom.getNumBytes();
 
-        if (writeBack > 0)
-            in.unread(buff, bom.getNumBytes(), writeBack);
+        if (bytesLeft > 0)
+            in.unread(buff, bom.getNumBytes(), bytesLeft);
 
         return bom;
     }
@@ -218,8 +219,8 @@ public class BOMInputStreamReader extends Reader {
         Objects.requireNonNull(cs, "Charset must not be null");
 
         Charset set = switch (getBOM()) {
-            case UTF_32_BE -> Charset.forName("UTF-32BE");
-            case UTF_32_LE -> Charset.forName("UTF-32LE");
+            case UTF_32_BE -> Charset.forName("UTF_32BE");
+            case UTF_32_LE -> Charset.forName("UTF_32LE");
             case UTF_8 -> StandardCharsets.UTF_8;
             case UTF_16_BE -> StandardCharsets.UTF_16BE;
             case UTF_16_LE -> StandardCharsets.UTF_16LE;
@@ -227,5 +228,333 @@ public class BOMInputStreamReader extends Reader {
         };
 
         return StreamDecoder.forInputStreamReader(in, this, set);
+    }
+
+    @SuppressWarnings("SynchronizeOnNonFinalField")
+    private static class StreamDecoder extends Reader {
+        public static StreamDecoder forInputStreamReader(InputStream in, Object lock, String charsetName) throws UnsupportedEncodingException {
+            String csn = charsetName;
+
+            if (csn == null) {
+                csn = Charset.defaultCharset().name();
+            }
+
+            try {
+                return new StreamDecoder(in, lock, Charset.forName(csn));
+            } catch (IllegalCharsetNameException | UnsupportedCharsetException x) {
+                throw new UnsupportedEncodingException (csn);
+            }
+        }
+
+        public static StreamDecoder forInputStreamReader(InputStream in, Object lock, Charset cs) {
+            return new StreamDecoder(in, lock, cs);
+        }
+
+        public static StreamDecoder forInputStreamReader(InputStream in, Object lock, CharsetDecoder dec) {
+            return new StreamDecoder(in, lock, dec);
+        }
+
+        public static StreamDecoder forDecoder(ReadableByteChannel ch, CharsetDecoder dec, int minBufferCap) {
+            return new StreamDecoder(ch, dec, minBufferCap);
+        }
+
+        public String getEncoding() {
+            if (isOpen())
+                return encodingName();
+
+            return null;
+        }
+
+        @Override
+        public int read() throws IOException {
+            return read0();
+        }
+
+        @Override
+        public int read(char[] buf, int offset, int length) throws IOException {
+            int off = offset;
+            int len = length;
+
+            synchronized (lock) {
+                ensureOpen();
+                if ((off < 0) || (off > buf.length) || (len < 0) || ((off + len) > buf.length) || ((off + len) < 0)) {
+                    throw new IndexOutOfBoundsException();
+                }
+
+                if (len == 0)
+                    return 0;
+
+                int n = 0;
+
+                if (haveLeftoverChar) {
+                    // Copy the leftover char into the buffer:
+                    //
+                    buf[off] = leftoverChar;
+                    off++; len--;
+                    haveLeftoverChar = false;
+                    n = 1;
+
+                    if ((len == 0) || !implReady())
+                        // Return now if this is all we can produce w/o blocking:
+                        //
+                        return n;
+                }
+
+                if (len == 1) {
+                    // Treat single-character array reads just like read():
+                    //
+                    int c = read0();
+
+                    if (c == -1)
+                        return (n == 0) ? -1 : n;
+
+                    buf[off] = (char)c;
+
+                    return n + 1;
+                }
+
+                return n + implRead(buf, off, off + len);
+            }
+        }
+
+        @Override
+        public boolean ready() throws IOException {
+            synchronized (lock) {
+                ensureOpen();
+                return haveLeftoverChar || implReady();
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            synchronized (lock) {
+                if (closed)
+                    return;
+
+                try {
+                    implClose();
+                } finally {
+                    closed = true;
+                }
+            }
+        }
+
+        private static final int MIN_BYTE_BUFFER_SIZE = 32;
+        private static final int DEFAULT_BYTE_BUFFER_SIZE = 8192;
+
+        private volatile boolean closed;
+
+        private final Charset cs;
+        private final CharsetDecoder decoder;
+        private final ByteBuffer bb;
+
+        private final InputStream in;
+        private final ReadableByteChannel ch;
+
+        private StreamDecoder(InputStream in, Object lock, Charset cs) {
+            this(in, lock, cs.newDecoder().onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE));
+        }
+
+        private StreamDecoder(InputStream in, Object lock, CharsetDecoder dec) {
+            super(lock);
+            this.cs = dec.charset();
+            this.decoder = dec;
+            this.in = in;
+            this.ch = null;
+            bb = ByteBuffer.allocate(DEFAULT_BYTE_BUFFER_SIZE);
+            bb.flip(); // So that bb is initially empty.
+        }
+
+        private StreamDecoder(ReadableByteChannel ch, CharsetDecoder dec, int mbc) {
+            this.in = null;
+            this.ch = ch;
+            this.decoder = dec;
+            this.cs = dec.charset();
+            this.bb = ByteBuffer.allocate(mbc < 0
+                    ? DEFAULT_BYTE_BUFFER_SIZE
+                    : (Math.max(mbc, MIN_BYTE_BUFFER_SIZE)));
+            bb.flip();
+        }
+
+        private void ensureOpen() throws IOException {
+            if (closed)
+                throw new IOException("Stream closed");
+        }
+
+        // In order to handle surrogates properly we must never try to produce
+        // fewer than two characters at a time.  If we're only asked to return one
+        // character then the other is saved here to be returned later:
+        //
+        private boolean haveLeftoverChar = false;
+        private char leftoverChar;
+
+        private int read0() throws IOException {
+            synchronized (lock) {
+                // Return the leftover char, if there is one:
+                //
+                if (haveLeftoverChar) {
+                    haveLeftoverChar = false;
+                    return leftoverChar;
+                }
+
+                // Convert more bytes:
+                //
+                char[] cb = new char[2];
+                int n = read(cb, 0, 2);
+                switch (n) {
+                    case -1:
+                        return -1;
+                    case 2:
+                        leftoverChar = cb[1];
+                        haveLeftoverChar = true;
+                        // FALL THROUGH!
+                    case 1:
+                        return cb[0];
+                    default:
+                        assert false : n;
+                        return -1;
+                }
+            }
+        }
+
+        private boolean isOpen() {
+            return !closed;
+        }
+
+        private int readBytes() throws IOException {
+            bb.compact();
+            try {
+                if (ch != null) {
+                    // Read from the channel:
+                    //
+                    int n = ch.read(bb);
+
+                    if (n < 0)
+                        return n;
+                } else {
+                    // Read from the input stream, and then update the buffer:
+                    //
+                    int lim = bb.limit();
+                    int pos = bb.position();
+
+                    assert (pos <= lim);
+
+                    int rem = lim - pos;
+                    int n = in.read(bb.array(), bb.arrayOffset() + pos, rem);
+
+                    if (n < 0)
+                        return n;
+
+                    if (n == 0)
+                        throw new IOException("Underlying input stream returned zero bytes");
+
+                    assert (n <= rem) : "n = " + n + ", rem = " + rem;
+
+                    bb.position(pos + n);
+                }
+            } finally {
+                // Flip even when an IOException is thrown,
+                // otherwise the stream will stutter:
+                //
+                bb.flip();
+            }
+
+            int rem = bb.remaining();
+            assert (rem != 0) : rem;
+            return rem;
+        }
+
+        int implRead(char[] cbuf, int off, int end) throws IOException {
+            // In order to handle surrogate pairs, this method requires that
+            // the invoker attempt to read at least two characters. Saving the
+            // extra character, if any, at a higher level is easier than trying
+            // to deal with it here.
+
+            assert (end - off > 1);
+
+            CharBuffer cb = CharBuffer.wrap(cbuf, off, end - off);
+            if (cb.position() != 0) {
+                // Ensure that cb[0] == buf[off]:
+                //
+                cb = cb.slice();
+            }
+
+            boolean eof = false;
+
+            for (;;) {
+                CoderResult cr = decoder.decode(bb, cb, eof);
+
+                if (cr.isUnderflow()) {
+                    if (eof)
+                        break;
+
+                    if (!cb.hasRemaining())
+                        break;
+
+                    if ((cb.position() > 0) && !inReady())
+                        break; // Block at most once.
+
+                    int n = readBytes();
+
+                    if (n < 0) {
+                        eof = true;
+
+                        if ((cb.position() == 0) && (!bb.hasRemaining()))
+                            break;
+
+                        decoder.reset();
+                    }
+
+                    continue;
+                }
+
+                if (cr.isOverflow()) {
+                    assert cb.position() > 0;
+                    break;
+                }
+
+                cr.throwException();
+            }
+
+            if (eof) {
+                // Need to flush decoder:
+                //
+                decoder.reset();
+            }
+
+            if (cb.position() == 0) {
+                if (eof) {
+                    return -1;
+                }
+
+                assert false;
+            }
+
+            return cb.position();
+        }
+
+        String encodingName() {
+            return cs.name();
+        }
+
+        private boolean inReady() {
+            try {
+                return (((in != null) && (in.available() > 0)) || (ch instanceof FileChannel)); // RBC.available()?
+            } catch (IOException x) {
+                return false;
+            }
+        }
+
+        boolean implReady() {
+            return bb.hasRemaining() || inReady();
+        }
+
+        void implClose() throws IOException {
+            if (ch != null) {
+                ch.close();
+            } else {
+                in.close();
+            }
+        }
     }
 }
---
+++ core/src/test/java/de/ganzer/core/io/BOMInputStreamReaderTest.java
@@ -0,0 +1,122 @@
+package de.ganzer.core.io;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BOMInputStreamReaderTest {
+    private static final String text = "ÄÖÜäöüß123";
+
+    @BeforeAll
+    static void setUpBeforeClass() {
+        writeASCII();
+        writeUTF8BOM();
+        writeUTF16BEBOM();
+        writeUTF16LEBOM();
+        writeUTF32BEBOM();
+        writeUTF32LEBOM();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @AfterAll
+    static void tearDownAfterClass() {
+        new File(StandardCharsets.ISO_8859_1.name()).delete();
+        new File(StandardCharsets.UTF_8.name()).delete();
+        new File(StandardCharsets.UTF_16BE.name()).delete();
+        new File(StandardCharsets.UTF_16LE.name()).delete();
+        new File(Charset.forName("UTF_32BE").name()).delete();
+        new File(Charset.forName("UTF_32LE").name()).delete();
+    }
+
+    private static void writeASCII() {
+        write(StandardCharsets.ISO_8859_1, null);
+    }
+
+    private static void writeUTF8BOM() {
+        write(StandardCharsets.UTF_8, new byte[] {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF});
+    }
+
+    private static void writeUTF16BEBOM() {
+        write(StandardCharsets.UTF_16BE, new byte[] {(byte) 0xFE, (byte) 0xFF});
+    }
+
+    private static void writeUTF16LEBOM() {
+        write(StandardCharsets.UTF_16LE, new byte[] {(byte) 0xFF, (byte) 0xFE});
+    }
+
+    private static void writeUTF32BEBOM() {
+        write(Charset.forName("UTF_32BE"), new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0xFE, (byte) 0xFF});
+    }
+
+    private static void writeUTF32LEBOM() {
+        write(Charset.forName("UTF_32LE"), new byte[] {(byte) 0xFF, (byte) 0xFE, (byte) 0x00, (byte) 0x00});
+    }
+
+    private static void write(Charset cs, byte[] bom) {
+        try (var fos = new FileOutputStream(cs.name());
+             var osw = new OutputStreamWriter(fos, cs);
+        ) {
+            if (bom != null)
+                fos.write(bom, 0, bom.length);
+
+            osw.write(text);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private static String read(String filename) {
+        try (var fis = new FileInputStream(filename);
+             var isr = new BOMInputStreamReader(fis, StandardCharsets.ISO_8859_1);
+        ) {
+            var bir = new BufferedReader(isr);
+            return bir.readLine();
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return "";
+    }
+
+    @Test
+    void readASCII() {
+        String read = read(StandardCharsets.ISO_8859_1.name());
+        assertEquals(text, read);
+    }
+
+    @Test
+    void readUTF8() {
+        String read = read(StandardCharsets.UTF_8.name());
+        assertEquals(text, read);
+    }
+
+    @Test
+    void readUTF16BE() {
+        String read = read(StandardCharsets.UTF_16BE.name());
+        assertEquals(text, read);
+    }
+
+    @Test
+    void readUTF16LE() {
+        String read = read(StandardCharsets.UTF_16LE.name());
+        assertEquals(text, read);
+    }
+
+    @Test
+    void readUTF32BE() {
+        String read = read(Charset.forName("UTF_32BE").name());
+        assertEquals(text, read);
+    }
+
+    @Test
+    void readUTF32LE() {
+        String read = read(Charset.forName("UTF_32LE").name());
+        assertEquals(text, read);
+    }
+}
