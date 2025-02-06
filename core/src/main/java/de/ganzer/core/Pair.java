d3881d7f3b79e0712f03ab4c628a65117be225e5
AGanzer <ganzer@velocom.de>
2025-02-06 20:21:06 +0100

    added missing java doc

--- core/src/main/java/de/ganzer/core/Pair.java
+++ core/src/main/java/de/ganzer/core/Pair.java
@@ -2,23 +2,48 @@ package de.ganzer.core;
 
 import java.util.Objects;
 
+/**
+ * Defines a pair ov values.
+ *
+ * @param <F> The type of the first value.
+ * @param <S> The type of the second value.
+ */
 public class Pair<F, S> {
     private final F first;
     private final S second;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param first The first value to set.
+     * @param second The second value to set.
+     */
     public Pair(F first, S second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Gets the first value.
+     *
+     * @return The value that was set at construction.
+     */
     public F getFirst() {
         return first;
     }
 
+    /**
+     * Gets the second value.
+     *
+     * @return The value that was set at construction.
+     */
     public S getSecond() {
         return second;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -30,6 +55,9 @@ public class Pair<F, S> {
         return Objects.equals(first, op.first) && Objects.equals(second, op.second);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(first, second);
