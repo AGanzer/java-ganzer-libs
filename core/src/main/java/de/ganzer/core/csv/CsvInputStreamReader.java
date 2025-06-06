212584c979e5153fff7182caa76829440fb9c179
Andreas Ganzer <ganand@web.de>
2025-06-06 13:26:54 +0200

    changed: CsvInputStreamReader now extends BOMInputStreamReader

--- core/src/main/java/de/ganzer/core/csv/CsvInputStreamReader.java
+++ core/src/main/java/de/ganzer/core/csv/CsvInputStreamReader.java
@@ -5,10 +5,7 @@ import de.ganzer.core.io.BOMInputStreamReader;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 import java.util.ArrayList;
 import java.util.List;
 
