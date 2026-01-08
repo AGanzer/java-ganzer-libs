4eba0d4b92a1c9cfd1bfbb84b6dee7430606bd1b
AGanzer <ganzer@velocom.de>
2026-01-09 00:02:21 +0100

    finished LogTarget

--- core/src/main/java/de/ganzer/core/logging/FormattedLogTarget.java
+++ core/src/main/java/de/ganzer/core/logging/FormattedLogTarget.java
@@ -1,6 +1,7 @@
 package de.ganzer.core.logging;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * A base class for log targets that wont to write all log message information
@@ -8,11 +9,81 @@ import java.time.LocalDateTime;
  */
 public abstract class FormattedLogTarget extends LogTarget {
     /**
+     * Creates a new instance from the specified argument.
+     *
+     * @param level The level of the log messages to write.
+     *
+     * @see #FormattedLogTarget(int, int)
+     * @see #FormattedLogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public FormattedLogTarget(int level) {
+        super(level);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param filter the filter to use to determine whether a message is written.
+     *         If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *         used.
+     *
+     * @see #FormattedLogTarget(int, int)
+     * @see #FormattedLogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public FormattedLogTarget(int level, LogFilter filter) {
+        super(level, filter);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *         messages are joined to a single message. See {@link #write(LogInfo[])}
+     *         for a more detailed explanation. If this is less than 1,
+     *         incoming messages are not joined.
+     */
+    public FormattedLogTarget(int level, int messageWaitTimeout) {
+        super(level, messageWaitTimeout);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param filter the filter to use to determine whether a message is written.
+     *         If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *         used.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *         messages are joined to a single message. See {@link #write(LogInfo[])}
+     *         for a more detailed explanation. If this is less than 1,
+     *         incoming messages are not joined.
+     */
+    public FormattedLogTarget(int level, LogFilter filter, int messageWaitTimeout) {
+        super(level, filter, messageWaitTimeout);
+    }
+
+    /**
      * Called by {@link #write(int, LocalDateTime, String)} to write the messages
      * that are not discarded by a filter into the physical target.
      * <p>
-     * This implementation formats the messages and calls {@link #write(int, String)}
-     * to write it into a physical target.
+     * In some cases many single messages slows down the update behavior of
+     * the target or decreases the responsiveness of the application. Joined
+     * messages are not written directly into the target, but they are
+     * collected until the target is able to work further messages. These
+     * messages should be written as one single message.
+     * <p>
+     * If {@link #getMessageWaitTimeout()} is less than 1, {@code info} contains
+     * only one element. In all other cases it may contain multiple elements.
+     * These elements should be connected to one message in an implementation of
+     * this method to perform only a single write action for each message.
+     * <p>
+     * This implementation formats and joins the messages and calls
+     * {@link #write(int, String)} to write them into a physical target
+     * at once.
      *
      * @param info The information about the messages to write.
      */
--- core/src/main/java/de/ganzer/core/logging/LogTarget.java
+++ core/src/main/java/de/ganzer/core/logging/LogTarget.java
@@ -1,6 +1,14 @@
 package de.ganzer.core.logging;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * The LogTarget class defines an abstract target for log messages.
@@ -8,15 +16,119 @@ import java.time.LocalDateTime;
  * @see Logger
  */
 public abstract class LogTarget {
+    private final int level;
+    private final LogFilter filter;
+    private final int messageWaitTimeout;
+    private final Queue<LogInfo> pendingMessages;
+    private final ReentrantLock writeLock;
+    private final Condition writeCondition;
+
     private Logger owner;
     private int messageNumber;
+    private MessageWorker messageWorker;
+    private MessageWakeup messageWakeup;
 
-    final Logger getOwner() {
-        return owner;
+    /**
+     * Creates a new instance from the specified argument.
+     *
+     * @param level The level of the log messages to write.
+     *
+     * @see #LogTarget(int, int)
+     * @see #LogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public LogTarget(int level) {
+        this(level,null, 0);
     }
 
-    final void setOwner(Logger owner) {
-        this.owner = owner;
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param filter the filter to use to determine whether a message is written.
+     *        If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *        used.
+     *
+     * @see #LogTarget(int, int)
+     * @see #LogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public LogTarget(int level, LogFilter filter) {
+        this(level,filter, 0);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *        messages are joined to a single message. See {@link #write(LogInfo[])}
+     *        for a more detailed explanation. If this is less than 1,
+     *        incoming messages are not joined.
+     */
+    public LogTarget(int level, int messageWaitTimeout) {
+        this(level,null, messageWaitTimeout);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param filter the filter to use to determine whether a message is written.
+     *        If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *        used.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *        messages are joined to a single message. See {@link #write(LogInfo[])}
+     *        for a more detailed explanation. If this is less than 1,
+     *        incoming messages are not joined.
+     */
+    public LogTarget(int level, LogFilter filter, int messageWaitTimeout) {
+        this.level = level;
+        this.filter = filter != null ? filter : new DefaultLogFilter();
+        this.messageWaitTimeout = messageWaitTimeout;
+
+        if (messageWaitTimeout > 0) {
+            pendingMessages = new LinkedList<>();
+            writeLock = new ReentrantLock();
+            writeCondition = writeLock.newCondition();
+        } else  {
+            pendingMessages = null;
+            writeLock = null;
+            writeCondition = null;
+        }
+    }
+
+    /**
+     * Gets the level set at construction.
+     *
+     * @return The log level of this target.
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Gets the filter set at construction.
+     *
+     * @return The filter for the messages to write or {@link DefaultLogFilter}
+     *         if no filter was set.
+     */
+    public LogFilter getFilter() {
+        return filter;
+    }
+
+    /**
+     * Gets the timeout to wait for incoming messages.
+     *
+     * @return The timeout in milliseconds to wait for incoming messages. If
+     *         this is less than 1, each message is written as a single message.
+     *         See {@link #write(LogInfo[])} for a more detailed explanation.
+     *
+     * @see #LogTarget(int, int)
+     * @see #LogTarget(int, LogFilter, int)
+     */
+    public int getMessageWaitTimeout() {
+        return messageWaitTimeout;
     }
 
     /**
@@ -31,15 +143,127 @@ public abstract class LogTarget {
      * @param message The message to write.
      */
     public final void write(int level, LocalDateTime time, String message) {
+        if (!filter.shouldWrite(level, this.level))
+            return;
+
         var info = new LogInfo(++messageNumber, level, time, Thread.currentThread().getId(), Thread.currentThread().getName(), message);
-        write(new LogInfo[]{info});
+
+        if (messageWaitTimeout < 1)
+            write(new LogInfo[]{info});
+        else {
+            synchronized (pendingMessages) {
+                pendingMessages.add(info);
+            }
+        }
     }
 
     /**
      * Called by {@link #write(int, LocalDateTime, String)} to write the messages
      * that are not discarded by a filter into the physical target.
+     * <p>
+     * In some cases many single messages slows down the update behavior of
+     * the target or decreases the responsiveness of the application. Joined
+     * messages are not written directly into the target, but they are
+     * collected until the target is able to work further messages. These
+     * messages should be written as one single message.
+     * <p>
+     * If {@link #getMessageWaitTimeout()} is less than 1, {@code info} contains
+     * only one element. In all other cases it may contain multiple elements.
+     * These elements should be connected to one message in an implementation of
+     * this method to perform only a single write action for each message.
      *
      * @param info The information about the messages to write.
+     *
+     * @see #LogTarget(int, int)
+     * @see #LogTarget(int, LogFilter, int)
      */
     protected abstract void write(LogInfo[] info);
+
+    final Logger getOwner() {
+        return owner;
+    }
+
+    final void setOwner(Logger owner) {
+        if (this.owner != null) {
+            messageWakeup.cancel();
+            messageWorker.cancel();
+
+            try {
+                messageWakeup.wait();
+                messageWorker.wait();
+            } catch (InterruptedException e) {
+                // Ignore;
+            }
+        }
+
+        this.owner = owner;
+
+        if (this.owner != null) {
+            messageWorker = new MessageWorker();
+            messageWakeup = new MessageWakeup();
+
+            messageWorker.start();
+            messageWakeup.start();
+        }
+    }
+
+    private class MessageWorker extends Thread {
+        private final AtomicBoolean canceled = new AtomicBoolean(false);
+
+        @Override
+        public void run() {
+            writeLock.lock();
+
+            try {
+                while (!canceled.get()) {
+                    var avail = writeCondition.await(100, TimeUnit.MILLISECONDS);
+
+                    if (!avail)
+                        continue;
+
+                    List<LogInfo> infos;
+
+                    synchronized (pendingMessages) {
+                        infos = new ArrayList<>(pendingMessages);
+                    }
+
+                    if (!infos.isEmpty())
+                        write(infos.toArray(new LogInfo[0]));
+                }
+            } catch (InterruptedException e) {
+                // Ignore.
+            } finally {
+                writeLock.unlock();
+            }
+        }
+
+        public void cancel() {
+            canceled.set(true);
+        }
+    }
+
+    private class MessageWakeup extends Thread {
+        private final AtomicBoolean canceled = new AtomicBoolean(false);
+
+        @Override
+        public void run() {
+            writeLock.lock();
+
+            try {
+                while (!canceled.get()) {
+                    //noinspection BusyWait
+                    Thread.sleep(messageNumber);
+                    writeCondition.signal();
+                }
+            } catch (InterruptedException e) {
+                // Ignore.
+            } finally {
+                writeLock.unlock();
+            }
+        }
+
+        public void cancel() {
+            canceled.set(true);
+        }
+    }
 }
