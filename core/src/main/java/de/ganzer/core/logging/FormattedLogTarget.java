890635b23663cb867bdd0b2bd681581d697879b8
AGanzer <ganzer@velocom.de>
2026-01-09 16:13:50 +0100

    finished FormattedLogTarget

--- core/src/main/java/de/ganzer/core/logging/FormattedLogTarget.java
+++ core/src/main/java/de/ganzer/core/logging/FormattedLogTarget.java
@@ -8,8 +8,12 @@ import java.util.List;
  * with a single preformatted string.
  */
 public abstract class FormattedLogTarget extends LogTarget {
+    private final LogFormatter formatter;
+
     /**
-     * Creates a new instance from the specified argument.
+     * Creates a new instance from the specified argument with a formatter of
+     * type {@link DefaultLogFormatter}, a filter of an instance of
+     * {@link DefaultLogFilter} and a message wait timeout of 0.
      *
      * @param level The level of the log messages to write.
      *
@@ -18,11 +22,12 @@ public abstract class FormattedLogTarget extends LogTarget {
      * @see #getMessageWaitTimeout()
      */
     public FormattedLogTarget(int level) {
-        super(level);
+        this(level, null, null, 0);
     }
 
     /**
-     * Creates a new instance from the specified arguments.
+     * Creates a new instance from the specified arguments with a formatter of
+     * type {@link DefaultLogFormatter} and a message wait timeout of 0.
      *
      * @param level The level of the log messages to write.
      * @param filter the filter to use to determine whether a message is written.
@@ -34,11 +39,13 @@ public abstract class FormattedLogTarget extends LogTarget {
      * @see #getMessageWaitTimeout()
      */
     public FormattedLogTarget(int level, LogFilter filter) {
-        super(level, filter);
+        this(level, null, filter, 0);
     }
 
     /**
-     * Creates a new instance from the specified arguments.
+     * Creates a new instance from the specified arguments with a formatter of
+     * type {@link DefaultLogFormatter} and filter of an instance of
+     * {@link DefaultLogFilter}.
      *
      * @param level The level of the log messages to write.
      * @param messageWaitTimeout The timeout in milliseconds to wait until
@@ -47,11 +54,12 @@ public abstract class FormattedLogTarget extends LogTarget {
      *         incoming messages are not joined.
      */
     public FormattedLogTarget(int level, int messageWaitTimeout) {
-        super(level, messageWaitTimeout);
+        this(level, null, null, messageWaitTimeout);
     }
 
     /**
-     * Creates a new instance from the specified arguments.
+     * Creates a new instance from the specified arguments with a formatter of
+     * type {@link DefaultLogFormatter}.
      *
      * @param level The level of the log messages to write.
      * @param filter the filter to use to determine whether a message is written.
@@ -63,7 +71,78 @@ public abstract class FormattedLogTarget extends LogTarget {
      *         incoming messages are not joined.
      */
     public FormattedLogTarget(int level, LogFilter filter, int messageWaitTimeout) {
+        this(level, null, filter, messageWaitTimeout);
+    }
+    /**
+     * Creates a new instance from the specified argument with a formatter of
+     * type {@link DefaultLogFormatter}, a filter of an instance of
+     * {@link DefaultLogFilter} and a message wait timeout of 0.
+     *
+     * @param level The level of the log messages to write.
+     * @param formatter The formatter to use. If this is {@code null}, an instance
+     *        of {@link DefaultLogFormatter} is used.
+     *
+     * @see #FormattedLogTarget(int, int)
+     * @see #FormattedLogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public FormattedLogTarget(int level, LogFormatter formatter) {
+        this(level, formatter, null, 0);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments with a formatter of
+     * type {@link DefaultLogFormatter} and a message wait timeout of 0.
+     *
+     * @param level The level of the log messages to write.
+     * @param formatter The formatter to use. If this is {@code null}, an instance
+     *        of {@link DefaultLogFormatter} is used.
+     * @param filter the filter to use to determine whether a message is written.
+     *         If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *         used.
+     *
+     * @see #FormattedLogTarget(int, int)
+     * @see #FormattedLogTarget(int, LogFilter, int)
+     * @see #getMessageWaitTimeout()
+     */
+    public FormattedLogTarget(int level, LogFormatter formatter, LogFilter filter) {
+        this(level, formatter, filter, 0);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments with a formatter of
+     * type {@link DefaultLogFormatter} and filter of an instance of
+     * {@link DefaultLogFilter}.
+     *
+     * @param level The level of the log messages to write.
+     * @param formatter The formatter to use. If this is {@code null}, an instance
+     *        of {@link DefaultLogFormatter} is used.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *         messages are joined to a single message. See {@link #write(LogInfo[])}
+     *         for a more detailed explanation. If this is less than 1,
+     *         incoming messages are not joined.
+     */
+    public FormattedLogTarget(int level, LogFormatter formatter, int messageWaitTimeout) {
+        this(level, formatter, null, messageWaitTimeout);
+    }
+
+    /**
+     * Creates a new instance from the specified arguments.
+     *
+     * @param level The level of the log messages to write.
+     * @param formatter The formatter to use. If this is {@code null}, an instance
+     *        of {@link DefaultLogFormatter} is used.
+     * @param filter the filter to use to determine whether a message is written.
+     *         If this is {@code null}, an instance of {@link DefaultLogFilter} is
+     *         used.
+     * @param messageWaitTimeout The timeout in milliseconds to wait until
+     *         messages are joined to a single message. See {@link #write(LogInfo[])}
+     *         for a more detailed explanation. If this is less than 1,
+     *         incoming messages are not joined.
+     */
+    public FormattedLogTarget(int level, LogFormatter formatter, LogFilter filter, int messageWaitTimeout) {
         super(level, filter, messageWaitTimeout);
+        this.formatter = formatter != null ? formatter : new DefaultLogFormatter();
     }
 
     /**
@@ -81,14 +160,24 @@ public abstract class FormattedLogTarget extends LogTarget {
      * These elements should be connected to one message in an implementation of
      * this method to perform only a single write action for each message.
      * <p>
-     * This implementation formats and joins the messages and calls
-     * {@link #write(int, String)} to write them into a physical target
-     * at once.
+     * This implementation formats and joins the messages (separated by '\n')
+     * and calls {@link #write(int, String)} to write them into a physical
+     * target at once.
      *
      * @param info The information about the messages to write.
      */
     @Override
     protected final void write(LogInfo[] info) {
+        StringBuilder sb = new StringBuilder();
+
+        for (var i = 0; i < info.length; i++) {
+            if (i > 0)
+                sb.append('\n');
+
+            sb.append(formatter.format(info[i]));
+        }
+
+        write(info.length == 1 ? info[0].getLevel() : Integer.MIN_VALUE, sb.toString());
     }
 
     /**
